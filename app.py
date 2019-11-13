a49faef34a9f97bc79e427a2fad97e48658b70c8
Francis Tsang <frtsang40@gmail.com>
2019-11-12 21:49:07 -0800

    weather reuslts

--- app.py
+++ app.py
@@ -14,28 +14,30 @@ def index():
 
 @app.route('/weather', methods=['GET', 'POST'])
 def weather():
-    return render_template('weather_form,html')
+    return render_template('weather_form.html')
 
 @app.route('/weather_results')
 def weather_results():
     user_city = request.args.get('city')
     API_KEY = '8f23d9ba5e589062e1110c726b2bdb57'
     
-    weather = {
+    params = {
         'q' : user_city,
         'appid': API_KEY
     }
     
-    r = requests.get(weather_url, weather=weather)
+    r = requests.get(weather_url, params=params)
 
     if not r.status_code == 200:
         print("error")
     results = r.json()
     city = results['name']
+    temp = convertions(results['main']['temp'])
+    return render_template('results.html', city=city, temp=temp)
 
     # print("It is now " + str(temp_in_kelvin) + " degrees in kelvin.")
 
-    return render_template('weather_results.html', weather=weather)
+    # return render_template('results.html', weather=weather)
 
 
 def convertions(temp):
